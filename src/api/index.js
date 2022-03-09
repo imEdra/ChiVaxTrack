a9967d3bebc05197a1caa42ad36ccee27fe8fbfb
Marc Bernardino <marcbernardino2005@gmail.com>
2022-03-08 19:28:00 -0600

    Update api
    json broky

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const url = 'https://data.cityofchicago.org/resource/2vhs-cf6b.json/';
     try {
       const { data } = await axios.get(url);
   
-      return data.map(({ total_doses_cumulative, _1st_dose_daily, _1st_dose_cumulative, vaccine_series_completed_daily, vaccine_series_completed_cumulative}) => ({ total_doses_cumulative, _1st_dose_daily, _1st_dose_cumulative, vaccine_series_completed_daily, vaccine_series_completed_cumulative }))[0];
+      return data.map(({ total_doses_cumulative, _1st_dose_daily, _1st_dose_cumulative, vaccine_series_completed_daily, vaccine_series_completed_cumulative}) => ({ total_doses_cumulative, _1st_dose_daily, _1st_dose_cumulative, vaccine_series_completed_daily, vaccine_series_completed_cumulative }))[1];
     } catch (error) {
       return error;
     }
