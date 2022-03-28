69a56273a178e7a7eeb9e40d07ec31108c50cfd9
Marc Bernardino <marcbernardino2005@gmail.com>
2022-03-28 08:48:15 -0500

    Update VaccineInfo.js

--- src/pages/VaccineInfo.js
+++ src/pages/VaccineInfo.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../partials/Header';
 
-function VaccineInfo() {
-  return ()
- // tba
-}
+function VaccineInfo() {};
 
 export default VaccineInfo;
