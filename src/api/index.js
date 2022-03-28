83e5ae3f06364dc0d1d8ebeec5b15e78e0aeac2c
Marc Bernardino <marcbernardino2005@gmail.com>
2022-03-28 09:08:33 -0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const url = 'https://data.cityofchicago.org/resource/2vhs-cf6b.json/';
  const fetchDailyData = async () => {
     try {
       const { data } = await axios.get(url);
-      const sortedData = await data.slice().sort((a, b) => b.calendar_date - a.calendar_date);
+      const sortedData = await data.slice().sort((a, b) => b.date - a.date);
       return sortedData.map(({ total_doses_cumulative, _1st_dose_daily, _1st_dose_cumulative, vaccine_series_completed_daily, vaccine_series_completed_cumulative}) => ({ total_doses_cumulative, _1st_dose_daily, _1st_dose_cumulative, vaccine_series_completed_daily, vaccine_series_completed_cumulative }))[0];
     } catch (error) {
       return error;
